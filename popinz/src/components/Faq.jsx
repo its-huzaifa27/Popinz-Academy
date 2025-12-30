1d6e6f788b006e18c31655520eac691552073f0e
MohammadM01 <mullamohammad123@gmail.com>
2025-12-30 14:18:54 +0530

    Footer fixed

--- popinz/src/components/Faq.jsx
+++ popinz/src/components/Faq.jsx
@@ -63,7 +63,7 @@ export function Faq() {
                         initial={{ opacity: 0, y: 20 }}
                         whileInView={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.6, delay: 0.1 }}
-                        className="space-y-6"
+                        className="space-y-6 lg:justify-self-center"
                     >
                         <h4 className="text-lg font-bold pb-4 inline-block">Popular Courses</h4>
                         <ul className="space-y-4">
@@ -85,7 +85,7 @@ export function Faq() {
                         initial={{ opacity: 0, y: 20 }}
                         whileInView={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.6, delay: 0.3 }}
-                        className="space-y-6"
+                        className="space-y-6 lg:justify-self-end"
                     >
                         <h4 className="text-lg font-bold pb-4 inline-block">Contact Us</h4>
                         <div className="space-y-4">
